f1084d6db93ba5b76bf41137483ee2055c3c3ad8
Una <askoike@gmail.com>
2024-10-11 12:25:19 -0700

    wk5 p5.js

---
+++ Week5/scripts/sketch.js
@@ -0,0 +1,35 @@
+let radius;
+let numCircles = 10;
+let numRings = 10;
+let strWeight = 20;
+let timerID
+let bkgClr
+
+let posX, posY;
+
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB, TWO_PI, 1, 1);
+
+  radius = width * 0.1;
+  strokeCap(SQUARE);
+  bkgClr = color(0)
+  timerID = setInterval(()=>{
+  bkgClr = color(random(TWO_PI), random(1), random(1))
+  } ,2000)
+}
+
+function draw() {
+  strokeWeight(strWeight);
+  background(bkgClr)
+  noFill();
+  translate(width / 2, height / 2);
+  for (let i = 0; i < numCircles; i++) {
+    push();
+    rotate(sin(millis() * 0.001 * (i * 0.5 + 1)));
+    let diameter = (i * strWeight * 2) + radius * 2;
+    stroke((TWO_PI / numCircles) * i, 1, 1);
+    arc(0, 0, diameter, diameter, HALF_PI + QUARTER_PI, QUARTER_PI);
+    pop();
+  }
+}
